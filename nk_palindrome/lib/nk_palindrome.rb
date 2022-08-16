9d7dabb496bbaa60fb6abcb911f1bb6ab8fcf069
Ubuntu <ubuntu@ip-172-31-7-109.us-west-2.compute.internal>
2022-08-16 03:34:18 +0000

    publishing gem

---
+++ nk_palindrome/lib/nk_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nk_palindrome/version"
+
+module NkPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ nk_palindrome/lib/nk_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NkPalindrome
+  VERSION = "0.1.0"
+end
